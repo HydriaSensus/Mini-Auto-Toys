d79cda19f881f5053449bb5eefd3db48ab33330c
HydriaSensus <hydriasensus@gmail.com>
2025-12-02 21:24:21 -0300

    Setup Scene
    Made some Resources for Testing Battle

--- Scenes/ui_battle.gd
+++ Scenes/ui_battle.gd
@@ -8,25 +8,25 @@ const SOLDIER = preload("uid://dx70tur8hxbfg")
 var new_instance = UI_PET.instantiate()
 
  #Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-
-	for i in range(5):
-		add_pet(load("res://sprites/turnicon.png"),i,false)
-		add_pet(SOLDIER,i,true)
-	
-	#Summon
-	#var pos=4
-	#add_pet(load("res://sprites/turnicon.png"),5,false)
-	#player_team.move_child(player_team.get_child(1),pos+1)
-	#player_team.get_child(pos).queue_free()
-	
-func add_pet(sprite,level,isEnemy):
-	new_instance = UI_PET.instantiate()
-	new_instance.sprite = sprite
-	new_instance.level=level+1
-	if isEnemy:
-		new_instance.enemy = true
-		enemy_team.add_child(new_instance,true)
-	else:
-		player_team.add_child(new_instance,true)
-		player_team.move_child(new_instance,1)
+#func _ready() -> void:
+#
+	#for i in range(5):
+		#add_pet(load("res://sprites/turnicon.png"),i,false)
+		#add_pet(SOLDIER,i,true)
+	#
+	##Summon
+	##var pos=4
+	##add_pet(load("res://sprites/turnicon.png"),5,false)
+	##player_team.move_child(player_team.get_child(1),pos+1)
+	##player_team.get_child(pos).queue_free()
+	#
+#func add_pet(sprite,level,isEnemy):
+	#new_instance = UI_PET.instantiate()
+	#new_instance.sprite = sprite
+	#new_instance.level=level+1
+	#if isEnemy:
+		#new_instance.enemy = true
+		#enemy_team.add_child(new_instance,true)
+	#else:
+		#player_team.add_child(new_instance,true)
+		#player_team.move_child(new_instance,1)
--- Scenes/ui_pet.gd
+++ Scenes/ui_pet.gd
@@ -13,14 +13,15 @@ extends VBoxContainer
 var is_enemy = false
 
 func _ready() -> void:
-	spriteNode.texture = toy.sprite
-	atk_label.text = str(toy.atk)
-	hp_label.text = str(toy.hp)
-	level_label.text = str(toy.level)
-	xp_bar.value = toy.xp
-#	item_sprite.texture =  item.sprite
-	if is_enemy == true:
-			spriteNode.flip_h = true
+	if toy:
+		spriteNode.texture = toy.sprite
+		atk_label.text = str(toy.atk)
+		hp_label.text = str(toy.hp)
+		level_label.text = str(toy.level)
+		xp_bar.value = toy.xp
+	#	item_sprite.texture =  item.sprite
+		if is_enemy == true:
+				spriteNode.flip_h = true
 
 #func _process(_delta: float) -> void:
 	#if sprite == null:
