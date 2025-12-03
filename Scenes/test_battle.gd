3f9faac0fa370277a266c6da2e143100a3e84ef2
HydriaSensus <hydriasensus@gmail.com>
2025-12-03 01:02:07 -0300

    Added turn timer
    the timer controls delay between attacks

--- Scenes/test_battle.gd
+++ Scenes/test_battle.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var enemy_team: HBoxContainer = $VBoxContainer/EnemyTeam
 @onready var player_team: HBoxContainer = $VBoxContainer/PlayerTeam
+@onready var turn_timer: Timer = $TurnTimer
 
 var player_list:Array
 var enemy_list:Array
@@ -27,28 +28,46 @@ func start_battle():
 	print("Battle starts")
 	print(player_list[0].toy.current_hp)
 	print(enemy_list[0].toy.current_hp)
-	while player_list.size() > 0 and enemy_list.size() > 0:
-		print(str("Player count: ",player_list.size()," Enemy count: ",enemy_list.size()))
-		resolve_turn()
-	if player_list.size() > enemy_list.size():
-		print("Player wins")
-	else: print("Player looses")
+	turn_timer.start()
+	#while player_list.size() > 0 and enemy_list.size() > 0:
+		#if turn_timer.is_stopped():
+			#print(str("Player count: ",player_list.size()," Enemy count: ",enemy_list.size()))
+			#turn_timer.start()
+			#print("Timer iniciado")
+		#resolve_turn()
+	#if player_list.size() > enemy_list.size():
+		#print("Player wins")
+	#else: print("Player looses")
 
 func resolve_turn()-> void:
-	var turno = 0
-	while player_list[0].toy.current_hp > 0 and enemy_list[0].toy.current_hp > 0:
-		turno +=1
-		print(turno)
-		enemy_list[0].toy.hurt(player_list[0].toy.atk)
-		player_list[0].toy.hurt(enemy_list[0].toy.atk)
-	
+	#var turno = 0
+	#while player_list[0].toy.current_hp > 0 and enemy_list[0].toy.current_hp > 0:
+		#turno +=1
+		#print(turno)
+		#enemy_list[0].toy.hurt(player_list[0].toy.atk)
+		#player_list[0].toy.hurt(enemy_list[0].toy.atk)
+	if player_list.size() > 0 and enemy_list.size() > 0:
+		turn_timer.start()
+	elif player_list.size() > enemy_list.size():
+		print("Player wins")
+	else: print("Player looses")
+	#if enemy_list[0].toy.current_hp <=0:
+		#enemy_list.pop_at(0).queue_free()
+		#print("enemigo fuera de lista")
+	#if player_list[0].toy.current_hp <= 0:
+		#player_list[0].pop_at(0).queue_free()
+		#print("jugador fuera de lista")
+
+func _on_turn_timer_timeout() -> void:
+	enemy_list[0].toy.hurt(player_list[0].toy.atk)
+	player_list[0].toy.hurt(enemy_list[0].toy.atk)
 	if enemy_list[0].toy.current_hp <=0:
 		enemy_list.pop_at(0).queue_free()
 		print("enemigo fuera de lista")
 	if player_list[0].toy.current_hp <= 0:
 		player_list[0].pop_at(0).queue_free()
 		print("jugador fuera de lista")
-
+	resolve_turn()
 
 	#Summon
 	#var pos=4
@@ -59,6 +78,7 @@ func resolve_turn()-> void:
 #func add_pet(sprite,level,isEnemy):
 	#new_instance = UI_PET.instantiate()
 	#new_instance.sprite = sprite
+
 	#new_instance.level=level+1
 	#if isEnemy:
 		#new_instance.enemy = true
